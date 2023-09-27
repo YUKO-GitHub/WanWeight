45b0057142f3c06c3ab28907cf2d0fb4a51a8b2d
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-09-27 14:28:07 +0900

    usersのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/index'
   root 'static_pages#home'
   get :terms, to: 'static_pages#terms'
   get :privacy_policy, to: 'static_pages#privacy_policy'
+  get :signup,       to: 'users#new'
+  resources :users
 end
