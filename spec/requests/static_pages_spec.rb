6b6a8a5e75f21636f14b4beeec7c50ddcee3a498
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-09-27 13:25:09 +0900

    rubocopで修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "StaticPages", type: :request do
       expect(response).to have_http_status "200"
     end
   end
-  
+
   describe "GET #terms" do
     it "正常なレスポンスを返すこと" do
       get terms_path
@@ -16,7 +16,7 @@ RSpec.describe "StaticPages", type: :request do
       expect(response).to have_http_status "200"
     end
   end
- 
+
   describe "GET #privacy_policy" do
     it "正常なレスポンスを返すこと" do
       get privacy_policy_path
