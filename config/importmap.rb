5b3e5baeb5d5e3efc221034bb28f508914533fee
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-09-30 15:17:26 +0900

    Alpinejsを使えるように設定し、ヘッダーのトグルメニューを有効化

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.13.0/dist/module.esm.js"
+pin "alpine-turbo-drive-adapter", to: "https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.1.0/dist/alpine-turbo-drive-adapter.esm.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
