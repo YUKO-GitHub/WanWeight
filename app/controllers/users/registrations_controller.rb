39c4096db7ce70112632e3db85c0fcfd02929dc2
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-04 11:22:10 +0900

    ログイン後のパスをマイページに設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -37,7 +37,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  # The path used after sign up.
   def after_sign_up_path_for(resource)
     mypage_path(resource)
   end
