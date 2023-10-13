f4751b66a61a2120b6c84c19c2027b6899becbc7
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-13 15:14:25 +0900

    Dogモデルのバリデーションを設定

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,10 @@
 class Dog < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :birthday, presence: true
+  validates :sex, inclusion: { in: Dog.sexes.keys }
+
+  enum sex: { unselected: 0, female: 1, male: 2 }
+
 end
