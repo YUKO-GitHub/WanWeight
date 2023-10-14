7292427875ef1863b4d6faa42adc73a15fa614a6
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-14 10:43:24 +0900

    マイページに愛犬の情報が表示されるように簡単実装

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @dog = current_user.dogs.build
+  end
+
+  def show
+  end
+
+  def create
+    @dog = current_user.dogs.build(dog_params)
+    if @dog.save
+      redirect_to some_path, notice: '愛犬を登録しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to some_path, notice: '愛犬の情報を更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to some_path, notice: '愛犬の情報を削除しました'
+  end
+
+  private
+
+  def set_dog
+    @dog = current_user.dogs.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :birthday, :sex, :breed, :profile_image)
+  end
 end
