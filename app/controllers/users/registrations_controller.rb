60ee9f7baa96cc0fcf2475bce436b140936546bb
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-18 12:19:11 +0900

    アカウント削除機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,4 +4,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     mypage_path(resource)
   end
+
+  def after_destroy_path_for(resource)
+    root_path
+  end
 end
