4834bbe93dde36e690ad620bfc340d137232de0a
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-19 16:25:28 +0900

    メールアドレスを小文字に変換するメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   has_many :dogs, dependent: :destroy
 
   # Include default devise modules. Others available are:
@@ -9,7 +10,13 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, length: { maximum: 255 }
+  validates :email, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
   validates :introduction, length: {maximum: 50}
+
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
 end
