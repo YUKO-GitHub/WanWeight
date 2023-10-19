a29a11b993b47559017c93986b7b804e5e64fb15
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-19 14:05:59 +0900

    パスワードは英数字を含む必要があるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
   validates :name, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+
 end
