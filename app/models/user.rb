f8a8b87ab612765da9dbd15c9c8c21755bc29cf3
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-19 15:48:01 +0900

    メールアドレスのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, length: { maximum: 255 }
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
-
+  validates :introduction, length: {maximum: 50}
 end
