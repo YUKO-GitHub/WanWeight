0c0e1ae20484d5b2d4d7a198021da6fa0e4967cf
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-22 15:37:04 +0900

    Userのモデルスペックを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
+    birthday { 20.years.ago }
+    email { Faker::Internet.email }
+    password { 'password123' }
+    introduction { Faker::Lorem.sentence }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,47 +3,64 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
-  # 存在性のバリデーション
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:birthday) }
-
-  # 長さのバリデーション
-  it { is_expected.to validate_length_of(:name).is_at_most(50) }
-  it { is_expected.to validate_length_of(:email).is_at_most(255) }
-  it { is_expected.to validate_length_of(:introduction).is_at_most(50) }
-
-  # 一意性のバリデーション
-  describe 'email address uniqueness' do
-    let!(:existing_user) { create(:user, email: 'example@example.com') }
-    it 'is invalid with a duplicate email address' do
-      user.email = 'example@example.com'
+  describe 'validations' do
+    it '名前が空欄だと無効であること' do
+      user.name = nil
       expect(user).to_not be_valid
+      expect(user.errors[:name]).to include("を入力してください")
     end
-    it 'is valid with a unique email address' do
-      user.email = 'unique@example.com'
-      expect(user).to be_valid
+
+    it '誕生日が空欄だと無効であること' do
+      user.birthday = nil
+      expect(user).to_not be_valid
+      expect(user.errors[:birthday]).to include("を入力してください")
     end
-  end
 
-  # メールアドレスの小文字化
-  it 'downcases email before saving' do
-    user.email = 'EXAMPLE@EXAMPLE.COM'
-    user.save!
-    expect(user.reload.email).to eq('example@example.com')
-  end
+    it '名前の長さに制限があること' do
+      user.name = 'a' * 51
+      expect(user).to_not be_valid
+      expect(user.errors[:name]).to include('は50文字以内で入力してください')
+    end
 
-  # パスワードのバリデーション
-  describe 'password validation' do
-    it 'is invalid if password is present and not alphanumeric' do
+    it 'メールアドレスの長さに制限があること' do
+      user.email = 'a' * 244 + '@example.com'
+      expect(user).to_not be_valid
+      expect(user.errors[:email]).to include('は255文字以内で入力してください')
+    end
+
+    it '自己紹介の長さに制限があること' do
+      user.introduction = 'a' * 51
+      expect(user).to_not be_valid
+      expect(user.errors[:introduction]).to include('は50文字以内で入力してください')
+    end
+
+    it 'メールアドレスが重複しないこと' do
+      user1 = create(:user, email: 'test@example.com')
+      user2 = build(:user, email: 'test@example.com')
+      expect(user2).to_not be_valid
+      expect(user2.errors[:email]).to include('はすでに存在します')
+    end
+
+    it 'パスワードの入力に制限があること' do
       user.password = 'password'
       expect(user).to_not be_valid
-    end
-    it 'is valid with an alphanumeric password' do
-      user.password = 'password123'
-      expect(user).to be_valid
+      expect(user.errors[:password]).to include('は半角英数を両方含む必要があります')
     end
   end
 
-  # アソシエーション
-  it { is_expected.to have_many(:dogs).dependent(:destroy) }
+  describe 'callbacks' do
+    it 'メールアドレスは小文字に変換してからDBに登録すること' do
+      user.email = 'EXAMPLE@EXAMPLE.COM'
+      user.save!
+      expect(user.reload.email).to eq('example@example.com')
+    end
+  end
+
+  describe 'associations' do
+    it 'ユーザーは複数のDogと関連があること' do
+      dog1 = create(:dog, user: user)
+      dog2 = create(:dog, user: user)
+      expect(user.dogs).to include(dog1, dog2)
+    end
+  end
 end
