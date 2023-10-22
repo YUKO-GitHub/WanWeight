01768ac802081ffaaa525c9af5f0237a7c3350a8
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-22 09:43:53 +0900

    ログイン時にメールアドレスを小文字に変換

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :downcase_email, only: :create
+
   protected
+
+  def downcase_email
+    if params[:user]
+      if params[:user][:email]
+        params[:user][:email] = params[:user][:email].downcase
+      end
+    end
+  end
 
   def after_sign_in_path_for(resource)
     mypage_path
