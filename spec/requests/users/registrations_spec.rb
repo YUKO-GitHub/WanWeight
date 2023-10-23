9d166a7f7b39569fdf781fc611655ce5fbf39467
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-23 13:03:13 +0900

    UserRegistrationのRequestSpecを作成

---
+++ spec/requests/users/registrations_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "UserRegistrations", type: :request do
+  describe "GET /sign_up" do
+    it "正常なレスポンスを返すこと" do
+      get new_user_registration_path
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "POST /sign_up" do
+    let(:user_params) do
+      {
+        name: "Example User",
+        birthday: "2000-01-01",
+        email: "user@example.com",
+        password: "password123",
+        password_confirmation: "password123",
+      }
+    end
+
+    context "パラメーターが有効な場合" do
+      it "新規ユーザーを作成し、マイページへリダイレクトすること" do
+        expect {
+          post user_registration_path, params: { user: user_params }
+        }.to change(User, :count).by(1)
+
+        expect(response).to redirect_to(mypage_path)
+      end
+    end
+
+    context "パラメーターが無効な場合" do
+      it "新規ユーザーは作成せず、新規登録のページを表示すること" do
+        user_params[:email] = ''
+
+        expect {
+          post user_registration_path, params: { user: user_params }
+        }.not_to change(User, :count)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include("メールアドレスを入力してください")
+      end
+    end
+  end
+end
