f0d415db451cd1e1f71dfadfc82af7ae09145c51
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-25 13:39:26 +0900

    アカウントページのRequest Specを作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  describe "GET /account" do
+    it "正常なレスポンスを返すこと" do
+      sign_in create(:user)
+      get user_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
