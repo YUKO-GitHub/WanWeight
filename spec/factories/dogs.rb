8d78336c8ad168c40ece35d3156e6552ee61ac3c
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-25 14:26:45 +0900

    DogのModel Specを作成

--- spec/factories/dogs.rb
+++ spec/factories/dogs.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :dog do
-    user { nil }
-    name { "MyString" }
-    birthday { "2023-10-13" }
-    sex { 1 }
-    breed { "MyString" }
-    profile_image { "MyString" }
+    association :user
+    name { "レオ" }
+    birthday { "2020-01-01" }
+    sex { "male" }
   end
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
+  let(:user) { create(:user) }
+  let(:dog) { create(:dog, user: user) }
+
+  it '名前がなければ無効な状態であること' do
+    dog.name = nil
+    expect(dog).to_not be_valid
+    expect(dog.errors[:name]).to include("を入力してください")
+  end
+
+  it '誕生日がなければ無効な状態であること' do
+    dog.birthday = nil
+    expect(dog).to_not be_valid
+    expect(dog.errors[:birthday]).to include("を入力してください")
+  end
+
+  it '性別が[unselected, female, male]のいずれかであること' do
+    expect(Dog.sexes.keys).to include(dog.sex)
+  end
+
+  it 'ユーザーと関連があること' do
+    expect(dog.user_id).to eq(user.id)
+  end
 end
