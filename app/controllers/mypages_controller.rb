98d1c4adaec4d11dfb7010320dc46679f4431fed
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-28 13:02:28 +0900

    タイムゾーンを修正し、日時表示のフォーマットを作成

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -7,6 +7,7 @@ class MypagesController < ApplicationController
 
     end_date = Time.current.to_date
     start_date = end_date - 1.month
-    @user_weights_for_month = current_user.user_weights.where(date: start_date..end_date).map { |weight| [weight.date, weight.weight] }.to_h
+    @user_weights_for_month = current_user.user_weights.where(
+      date: start_date.beginning_of_day..end_date.end_of_day).map { |weight| [weight.date.to_date, weight.weight] }.to_h
   end
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:custom_datetime] = '%Y年%m月%d日 %H:%M'
+Date::DATE_FORMATS[:custom_date] = '%Y年%m月%d日'
