dd0e00df30756d9d67e47cd6fb9e713c40900cc3
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-30 17:24:51 +0900

    体重の編集ページを作成

--- app/controllers/user_weights_controller.rb
+++ app/controllers/user_weights_controller.rb
@@ -1,10 +1,11 @@
 class UserWeightsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_weight, only: [:edit, :update, :destroy]
+  before_action :initialize_user_weight, only: [:create]
   before_action :combine_date_and_time, only: [:create, :update]
 
   def index
-    @year, @month = params[:date]&.split('-') || [Date.today.year, Date.today.month]
+    @year, @month = params[:date]&.split('-') || [Date.current.year, Date.current.month]
     start_date = Date.new(@year.to_i, @month.to_i, 1)
     end_date = start_date.end_of_month
 
@@ -17,7 +18,6 @@ class UserWeightsController < ApplicationController
   end
 
   def create
-    @user_weight = current_user.user_weights.new(user_weight_params.except(:date_part, :time_part))
     combine_date_and_time
     if @user_weight.save
       redirect_to mypage_path, notice: '体重が正常に記録されました。'
@@ -49,11 +49,17 @@ class UserWeightsController < ApplicationController
     params.require(:user_weight).permit(:weight, :date_part, :time_part)
   end
 
-  def combine_date_and_time
-    @user_weight.date = DateTime.parse("#{params[:user_weight][:date_part]} #{params[:user_weight][:time_part]}")
-  end
-
   def set_user_weight
     @user_weight = current_user.user_weights.find(params[:id])
+  end
+
+  def initialize_user_weight
+    @user_weight = current_user.user_weights.new(user_weight_params.except(:date_part, :time_part))
+  end
+
+  def combine_date_and_time
+    date = Date.parse(params[:user_weight][:date_part])
+    time = Time.zone.parse(params[:user_weight][:time_part])
+    @user_weight.date = Time.zone.local(date.year, date.month, date.day, time.hour, time.min, time.sec)
   end
 end
