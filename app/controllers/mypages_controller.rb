1dba53f18501aa15e3e1b34686e7bdf38b54a6ca
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-10-31 22:22:51 +0900

    rubocopで自動修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -8,6 +8,7 @@ class MypagesController < ApplicationController
     end_date = Time.current.to_date
     start_date = end_date - 1.month
     @user_weights_for_month = current_user.user_weights.where(
-      date: start_date.beginning_of_day..end_date.end_of_day).map { |weight| [weight.date.to_date, weight.weight] }.to_h
+      date: start_date.beginning_of_day..end_date.end_of_day
+    ).map { |weight| [weight.date.to_date, weight.weight] }.to_h
   end
 end
--- app/helpers/user_weights_helper.rb
+++ app/helpers/user_weights_helper.rb
@@ -2,7 +2,7 @@ module UserWeightsHelper
   def date_options
     options = []
     date = Date.today
-    while date >= Date.new(2020, 1)
+    while date >= Date.new(2023, 1)
       options << ["#{date.year}年#{date.month}月", "#{date.year}-#{date.month}"]
       date = date.prev_month
     end
