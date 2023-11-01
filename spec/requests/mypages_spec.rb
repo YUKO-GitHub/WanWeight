f850c75e2c441b1b545e7c4c1fb2038d1c83d4d9
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-01 15:53:32 +0900

    マイページのRequest SpecにUserWeightのテストを追加

--- spec/requests/mypages_spec.rb
+++ spec/requests/mypages_spec.rb
@@ -6,21 +6,39 @@ RSpec.describe "MypagesController", type: :request do
   let!(:dog) { create(:dog, user: user) }
 
   describe "GET /show" do
-    before do
-      sign_in user
-      get mypage_path
+    context "ログイン済みの場合" do
+      before do
+        sign_in user
+        get mypage_path
+      end
+
+      it "正常にレスポンスを返すこと" do
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+
+      it "愛犬情報がレスポンスに含まれていること" do
+        expect(response.body).to include dog.name
+        expect(response.body).to include dog.birthday.strftime('%Y-%m-%d')
+        expect(response.body).to include I18n.t("activerecord.enums.dog.sex.#{dog.sex}")
+        expect(response.body).to include dog.breed if dog.breed.present?
+      end
+
+      it "体重記録がレスポンスに含まれていること" do
+        expect(response.body).to include user_weight.weight.to_s
+        expect(response.body).to include user_weight.date.strftime('%Y-%m-%d')
+      end
+
     end
 
-    it "正常にレスポンスを返すこと" do
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
-    end
+    context "未ログインの場合" do
+      before do
+        get mypage_path
+      end
 
-    it "愛犬情報が表示されていること" do
-      expect(response.body).to include dog.name
-      expect(response.body).to include dog.birthday.strftime('%Y-%m-%d')
-      expect(response.body).to include I18n.t("activerecord.enums.dog.sex.#{dog.sex}")
-      expect(response.body).to include dog.breed if dog.breed.present?
+      it "ログインページへリダイレクトされること" do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 end
--- spec/system/mypages_spec.rb
+++ spec/system/mypages_spec.rb
@@ -14,6 +14,34 @@ RSpec.describe "マイページ", type: :system do
     expect(page).to have_title page_title('マイページ')
   end
 
+  context "ユーザーの体重記録が存在する場合" do
+    it "最新の体重記録が表示されること" do
+      expect(page).to have_content "前回の記録: #{user_weight.weight} kg"
+      expect(page).to have_content "記録日: #{user_weight.date.to_fs(:custom_datetime)}"
+    end
+
+    it "「体重を記録」と「体重を管理」のリンクが表示されること" do
+      expect(page).to have_link '体重を記録', href: new_user_weight_path
+      expect(page).to have_link '体重を管理', href: user_weights_path
+    end
+
+    it "体重のグラフが表示されること" do
+      expect(page).to have_css("script", text: "直近1ヶ月の体重推移", visible: false)
+    end
+  end
+
+  context "ユーザーの体重記録が存在しない場合" do
+    let!(:user_weight) { nil }
+
+    it "体重の記録がないというメッセージが表示されること" do
+      expect(page).to have_content "体重の記録がありません"
+    end
+
+    it "体重のグラフが表示されないこと" do
+      expect(page).to_not have_css("script", text: "直近1ヶ月の体重推移", visible: false)
+    end
+  end
+
   context "愛犬が登録されていない場合" do
     let!(:dog) { nil }
 
