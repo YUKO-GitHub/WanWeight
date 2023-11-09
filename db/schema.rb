3ce36b6d0d731ca8b86c5fbfea40925e7f8a9ed6
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-09 17:09:37 +0900

    Dogモデルのデータベースを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_070211) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_080805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,11 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_070211) do
 
   create_table "dogs", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "name"
-    t.date "birthday"
+    t.string "name", null: false
+    t.date "birthday", null: false
     t.integer "sex"
     t.string "breed"
-    t.string "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_dogs_on_user_id"
