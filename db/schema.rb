7de0d277767518ed2a8033b90aa3aec4f48fa23a
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-15 13:19:21 +0900

    Diaryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_080805) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_041439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_080805) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "diaries", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "dog_id"
+    t.datetime "date"
+    t.text "diary_text"
+    t.text "meal_text"
+    t.text "exercise_text"
+    t.text "health_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_diaries_on_dog_id"
+    t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
   create_table "dog_weights", force: :cascade do |t|
@@ -89,6 +103,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_080805) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "diaries", "dogs"
+  add_foreign_key "diaries", "users"
   add_foreign_key "dog_weights", "dogs"
   add_foreign_key "dogs", "users"
   add_foreign_key "user_weights", "users"
