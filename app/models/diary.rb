a51dfe2a5bbc33da1aa7b18ae7a8de2acfa98d71
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-21 16:24:22 +0900

    日記の文字数のバリデーションを設定

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,6 +3,11 @@ class Diary < ApplicationRecord
   belongs_to :dog, optional: true
   has_many_attached :photos
 
+  validates :diary_text, length: { maximum: 1000 }, allow_blank: true
+  validates :meal_text, length: { maximum: 1000 }, allow_blank: true
+  validates :exercise_text, length: { maximum: 1000 }, allow_blank: true
+  validates :health_text, length: { maximum: 1000 }, allow_blank: true
+
   validate :photos_count_within_limit
 
   private
