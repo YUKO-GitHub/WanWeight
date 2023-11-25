163c090b82e34ca6f81ead720e631e1c4b6a88eb
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-25 22:01:30 +0900

    DiaryモデルのModel Specを作成

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,4 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
+  let(:user) { create(:user) }
+  let(:dog) { create(:dog, user: user) }
+  let(:diary) { create(:diary, user: user, dog: dog) }
+  let(:dummy_image) { fixture_file_upload(Rails.root.join('spec/fixtures/files/sample_user.jpg'), 'image/jpeg') }
+
+  it 'ユーザーとの関連付けが存在すること' do
+    expect(diary.user_id).to eq(user.id)
+  end
+
+  it '犬との関連付けが存在すること' do
+    expect(diary.dog_id).to eq(dog.id)
+  end
+
+  it '日記テキストが1000文字以内であること' do
+    diary.diary_text = 'a' * 1001
+    expect(diary).to_not be_valid
+    expect(diary.errors[:diary_text]).to include("は1000文字以内で入力してください")
+  end
+
+  it '食事テキストが1000文字以内であること' do
+    diary.meal_text = 'a' * 1001
+    expect(diary).to_not be_valid
+    expect(diary.errors[:meal_text]).to include("は1000文字以内で入力してください")
+  end
+
+  it '運動テキストが1000文字以内であること' do
+    diary.exercise_text = 'a' * 1001
+    expect(diary).to_not be_valid
+    expect(diary.errors[:exercise_text]).to include("は1000文字以内で入力してください")
+  end
+
+  it '健康テキストが1000文字以内であること' do
+    diary.health_text = 'a' * 1001
+    expect(diary).to_not be_valid
+    expect(diary.errors[:health_text]).to include("は1000文字以内で入力してください")
+  end
+
+  it '写真が4枚までであること' do
+    5.times { diary.photos.attach(dummy_image) }
+    expect(diary).to_not be_valid
+    expect(diary.errors[:photos]).to include('は最大4枚までです。')
+  end
 end
