1fec86fa81b7260d18d9ca19154e7c9327629f7a
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-11-25 22:19:43 +0900

    Diaryモデルに画像の形式とサイズのバリデーションを設定

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -9,12 +9,23 @@ class Diary < ApplicationRecord
   validates :health_text, length: { maximum: 1000 }, allow_blank: true
 
   validate :photos_count_within_limit
+  validate :photos_format_and_size
 
   private
 
   def photos_count_within_limit
     if photos.length > 4
       errors.add(:photos, 'は最大4枚までです。')
+    end
+  end
+
+  def photos_format_and_size
+    photos.each do |photo|
+      if !photo.content_type.in?(%w(image/png image/jpg image/jpeg))
+        errors.add(:photos, 'はPNG、JPG、JPEG形式でアップロードしてください')
+      elsif photo.blob.byte_size > 5.megabytes
+        errors.add(:photos, 'のサイズは5MB以下にしてください')
+      end
     end
   end
 end
