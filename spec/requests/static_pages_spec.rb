102b7fdce1651ac0124e5b8cae794488fe70c60a
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-12-20 17:42:07 +0900

    テストコードの変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,20 +8,4 @@ RSpec.describe "StaticPagesController", type: :request do
       expect(response).to have_http_status "200"
     end
   end
-
-  describe "GET #terms" do
-    it "正常なレスポンスを返すこと" do
-      get terms_path
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
-    end
-  end
-
-  describe "GET #privacy_policy" do
-    it "正常なレスポンスを返すこと" do
-      get privacy_policy_path
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
-    end
-  end
 end
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -10,24 +10,4 @@ RSpec.describe "静的ページ", type: :system do
       expect(page).to have_title page_title('')
     end
   end
-
-  describe "terms" do
-    before do
-      visit terms_path
-    end
-
-    it "正しいタイトルが表示されること" do
-      expect(page).to have_title page_title('利用規約')
-    end
-  end
-
-  describe "privacy_policy" do
-    before do
-      visit privacy_policy_path
-    end
-
-    it "正しいタイトルが表示されること" do
-      expect(page).to have_title page_title('プライバシーポリシー')
-    end
-  end
 end
