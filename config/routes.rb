24a352b044f673b90fd070ee70805c012144c928
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2023-12-20 17:25:07 +0900

    ロゴマークを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get :terms, to: 'static_pages#terms'
-  get :privacy_policy, to: 'static_pages#privacy_policy'
   get :mypage, to: 'mypages#show'
   get 'account', to: 'users#show', as: 'user'
   get 'profiles', to: 'profiles#show'
