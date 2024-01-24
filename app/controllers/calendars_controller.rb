2b748daaea04310b80aa6fe1bc834faeef2b19dd
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2024-01-24 15:19:46 +0900

    カレンダーに表示する内容のデータをコントローラーで取得

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,9 @@
 class CalendarsController < ApplicationController
   def show
-    
+    user_weights = UserWeight.all
+    dog_weights = DogWeight.all
+    diaries = Diary.all
+
+    @events = user_weights + dog_weights + diaries
   end
 end
