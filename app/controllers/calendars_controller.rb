5c8ac04245fba8064fee4493e84d1b5bb50db209
YUKO-GitHub <yu-uk-mov.ie@hotmail.co.jp>
2024-01-30 16:09:04 +0900

    カレンダーにデータを表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,9 +1,12 @@
 class CalendarsController < ApplicationController
   def show
+    start_date = params.fetch(:start_date, Date.today).to_date
+
     user_weights = UserWeight.all
     dog_weights = DogWeight.all
     diaries = Diary.all
 
+    @start_date = start_date
     @events = user_weights + dog_weights + diaries
   end
 end
